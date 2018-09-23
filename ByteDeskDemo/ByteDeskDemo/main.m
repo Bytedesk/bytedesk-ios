ac36204d5237a9bdd495c111bc22030187fbbf39
jackning <270580156@qq.com>
2018-09-23 11:10:52 +0800

    update

---
+++ ByteDeskDemo/ByteDeskDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ByteDeskDemo
+//
+//  Created by 宁金鹏 on 2018/9/23.
+//  Copyright © 2018年 宁金鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ByteDeskDemo/Pods/Target Support Files/Pods-ByteDeskDemo/Pods-ByteDeskDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ByteDeskDemo : NSObject
+@end
+@implementation PodsDummy_Pods_ByteDeskDemo
+@end
